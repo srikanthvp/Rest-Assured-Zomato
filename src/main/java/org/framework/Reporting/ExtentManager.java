1103cb7796b42b1462d98d66ccb0ff26bdd07066
srikanthvp <svpglb@gmail.com>
2020-06-16 06:41:48 +0530

    Added Assertions Added Reporting Added positive test cases Added suite execution

--- src/main/java/org/framework/Reporting/ExtentManager.java
+++ src/main/java/org/framework/Reporting/ExtentManager.java
@@ -2,6 +2,7 @@ package org.framework.Reporting;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -9,16 +10,18 @@ import java.util.Date;
 public class ExtentManager {
 
     private static ExtentReports extent;
-    static String filepath = (System.getProperty("Jobname")!=null) ?
-            System.getProperty("user.dir")+"/reports/"+System.getProperty("Jobname")+"/Result.html" :
-            System.getProperty("user.dir")+"/reports/"+System.getProperty("Environment")+
-                    new SimpleDateFormat("_yyyy-MM-dd_HH-mm_").format(new Date())+".html";
+    static String filepath = System.getProperty("user.dir")+"/reports/"+ new SimpleDateFormat("yyyy-MM-dd-HH-mm_")
+                            .format(new Date())+System.getProperty("Environment")+".html";
 //    static String filepath =System.getProperty("user.dir")+"/reports/Result.html";
 
     static ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter(filepath);
 
     public synchronized static ExtentReports getInstance() {
         if (extent == null) {
+            htmlReporter.config().setDocumentTitle("API Automation Report");
+            htmlReporter.config().setReportName("Zomato API");
+            htmlReporter.config().setTheme(Theme.DARK);
+
             extent = new ExtentReports();
            extent.attachReporter(htmlReporter);
         }
